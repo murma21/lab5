4fd5b9e5c7ae0f628520c239ce1d9c709f6a69bc
murma21 <maksimys1301@mail.ru>
2024-04-16 17:13:16 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите операцию");
+            String operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(a+b);
+                    break;
+                case "-":
+                    Console.WriteLine(a-b);
+                    break;
+                case "*":
+                    Console.WriteLine(a*b);
+                    break;
+                case "/":
+                    Console.WriteLine(a/b);
+                    break;
+            }
+        }
+    }
+}
